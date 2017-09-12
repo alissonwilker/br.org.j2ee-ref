0683d76269741a49ab3efdd0a17ff76ec9e52191
Alisson Wilker <alissonwilker@gmail.com>
2017-09-12 09:33:57 -0300

    Implementação do eureka.

--- libros-negocio/src/main/java/br/org/libros/negocio/biblioteca/model/business/BibliotecaBusiness.java
+++ libros-negocio/src/main/java/br/org/libros/negocio/biblioteca/model/business/BibliotecaBusiness.java
@@ -1,15 +1,39 @@
 package br.org.libros.negocio.biblioteca.model.business;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.json.JsonArray;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInstanceConfig;
+import com.netflix.appinfo.providers.EurekaConfigBasedInstanceInfoProvider;
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.EurekaClientConfig;
+
 import br.org.arquitetura.excecao.EntidadeJaExisteExcecao;
 import br.org.arquitetura.excecao.EntidadeNaoEncontradaExcecao;
+import br.org.arquitetura.excecao.RuntimeExcecao;
 import br.org.arquitetura.model.business.AbstractBusiness;
 import br.org.libros.negocio.biblioteca.model.persistence.entity.Biblioteca;
 import br.org.libros.negocio.livrobiblioteca.model.persistence.entity.LivroBiblioteca;
@@ -22,6 +46,120 @@ import br.org.libros.negocio.livrobiblioteca.model.persistence.entity.LivroBibli
 @RequestScoped
 public class BibliotecaBusiness extends AbstractBusiness<Biblioteca, Integer> {
 
+    private static ApplicationInfoManager applicationInfoManager;
+    private static EurekaClient eurekaClient;
+
+    private static synchronized ApplicationInfoManager initializeApplicationInfoManager(EurekaInstanceConfig instanceConfig) {
+        if (applicationInfoManager == null) {
+            InstanceInfo instanceInfo = new EurekaConfigBasedInstanceInfoProvider(instanceConfig).get();
+            applicationInfoManager = new ApplicationInfoManager(instanceConfig, instanceInfo);
+        }
+
+        return applicationInfoManager;
+    }
+
+    private static synchronized EurekaClient initializeEurekaClient(ApplicationInfoManager applicationInfoManager, EurekaClientConfig clientConfig) {
+        if (eurekaClient == null) {
+            eurekaClient = new DiscoveryClient(applicationInfoManager, clientConfig);
+        }
+
+        return eurekaClient;
+    }
+
+
+    public void sendRequestToServiceUsingEureka(EurekaClient eurekaClient) {
+        // initialize the client
+        // this is the vip address for the example service to talk to as defined in conf/sample-eureka-service.properties
+        String vipAddress = "sampleservice.mydomain.net";
+
+        InstanceInfo nextServerInfo = null;
+        try {
+            nextServerInfo = eurekaClient.getNextServerFromEureka(vipAddress, false);
+            System.out.println("teste: " + nextServerInfo.getMetadata().get("teste"));
+        } catch (Exception e) {
+            System.err.println("Cannot get an instance of example service to talk to from eureka");
+            System.exit(-1);
+        }
+
+        System.out.println("Found an instance of example service to talk to from eureka: "
+                + nextServerInfo.getVIPAddress() + ":" + nextServerInfo.getPort());
+
+        System.out.println("healthCheckUrl: " + nextServerInfo.getHealthCheckUrl());
+        System.out.println("override: " + nextServerInfo.getOverriddenStatus());
+        System.out.println("nextServerInfo.getHostName(): " + nextServerInfo.getHostName());
+        System.out.println("nextServerInfo.getIPAddr(): " + nextServerInfo.getIPAddr());
+
+//        Socket s = new Socket();
+        int serverPort = nextServerInfo.getPort();
+        System.out.println("nextServerInfo.getPort(): " + nextServerInfo.getPort());
+        
+        testarRecuperarLivros(nextServerInfo.getHostName());
+//        try {
+//            s.connect(new InetSocketAddress(nextServerInfo.getHostName(), serverPort));
+//        } catch (IOException e) {
+//            System.err.println("Could not connect to the server :"
+//                    + nextServerInfo.getHostName() + " at port " + serverPort);
+//        } catch (Exception e) {
+//            System.err.println("Could not connect to the server :"
+//                    + nextServerInfo.getHostName() + " at port " + serverPort + "due to Exception " + e);
+//        }
+//        try {
+//            String request = "FOO " + new Date();
+//            System.out.println("Connected to server. Sending a sample request: " + request);
+//
+//            PrintStream out = new PrintStream(s.getOutputStream());
+//            out.println(request);
+//
+//            System.out.println("Waiting for server response..");
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(s.getInputStream()));
+//            String str = rd.readLine();
+//            if (str != null) {
+//                System.out.println("Received response from server: " + str);
+//                System.out.println("Exiting the client. Demo over..");
+//            }
+//            rd.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+    
+    private void testarRecuperarLivros(String hostName) {
+		// TODO substituir endereço hardcoded por um service discovery
+		Client client = ClientBuilder.newClient();
+        System.out.println("Testando resolucao do servidor..");
+		JsonArray jsonArrayResponse = client.target("http://" + hostName + ":8180/livraria/api/livros/")
+				.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+		ObjectMapper jsonToObjectMapper = new ObjectMapper();
+		List<LivroBiblioteca> livrosBibliotecas = null;
+		try {
+			livrosBibliotecas = jsonToObjectMapper.readValue(jsonArrayResponse.toString(),
+					new TypeReference<List<LivroBiblioteca>>() {
+					});
+		} catch (IOException e) {
+			throw new RuntimeExcecao(e);
+		}
+		
+		if (livrosBibliotecas != null) {
+			for (LivroBiblioteca livroBiblioteca : livrosBibliotecas) {
+				System.out.println(livroBiblioteca.getId());
+			}
+		}
+	}
+
+	@PostConstruct
+    public void init() {
+        // create the client
+        ApplicationInfoManager applicationInfoManager = initializeApplicationInfoManager(new MyDataCenterInstanceConfig());
+        EurekaClient client = initializeEurekaClient(applicationInfoManager, new DefaultEurekaClientConfig());
+
+        // use the client
+        sendRequestToServiceUsingEureka(client);
+
+        // shutdown the client
+        eurekaClient.shutdown();
+
+    }
+	
 	private static final long serialVersionUID = 1L;
 
 	@Override
--- livraria/src/main/java/br/org/livraria/livro/api/LivroApi.java
+++ livraria/src/main/java/br/org/livraria/livro/api/LivroApi.java
@@ -1,7 +1,18 @@
 package br.org.livraria.livro.api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 import java.net.URISyntaxException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,6 +20,17 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInstanceConfig;
+import com.netflix.appinfo.providers.EurekaConfigBasedInstanceInfoProvider;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.EurekaClientConfig;
 
 import br.org.arquitetura.api.AbstractApi;
 import br.org.arquitetura.excecao.EntidadeJaExisteExcecao;
@@ -24,7 +46,134 @@ import io.swagger.annotations.ApiParam;
  */
 @Path(LivroApi.PATH)
 @Api(LivroApi.PATH)
+@Singleton
 public class LivroApi extends AbstractApi<LivroDto, Integer> {
+	private static ApplicationInfoManager applicationInfoManager;
+	private static EurekaClient eurekaClient;
+	private DynamicPropertyFactory configInstance;
+
+	private static synchronized ApplicationInfoManager initializeApplicationInfoManager(
+			EurekaInstanceConfig instanceConfig) {
+		if (applicationInfoManager == null) {
+			InstanceInfo instanceInfo = new EurekaConfigBasedInstanceInfoProvider(instanceConfig).get();
+			applicationInfoManager = new ApplicationInfoManager(instanceConfig, instanceInfo);
+			Map<String, String> mapa = new HashMap<String, String>();
+			mapa.put("teste", "valorTeste");
+			applicationInfoManager.registerAppMetadata(mapa);
+		}
+
+		return applicationInfoManager;
+	}
+
+	private static synchronized EurekaClient initializeEurekaClient(ApplicationInfoManager applicationInfoManager,
+			EurekaClientConfig clientConfig) {
+		if (eurekaClient == null) {
+			eurekaClient = new DiscoveryClient(applicationInfoManager, clientConfig);
+		}
+
+		return eurekaClient;
+	}
+
+	@PostConstruct
+	public void start() {
+		configInstance = com.netflix.config.DynamicPropertyFactory.getInstance();
+		applicationInfoManager = initializeApplicationInfoManager(new MyDataCenterInstanceConfig());
+		eurekaClient = initializeEurekaClient(applicationInfoManager, new DefaultEurekaClientConfig());
+
+		// A good practice is to register as STARTING and only change status to
+		// UP
+		// after the service is ready to receive traffic
+		System.out.println("Registering service to eureka with STARTING status");
+		applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.STARTING);
+
+		System.out.println("Simulating service initialization by sleeping for 2 seconds...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// Nothing
+		}
+
+		// Now we change our status to UP
+		System.out.println("Done sleeping, now changing status to UP");
+		applicationInfoManager.setInstanceStatus(InstanceInfo.InstanceStatus.UP);
+		waitForRegistrationWithEureka(eurekaClient);
+		System.out.println("Service started and ready to process requests..");
+
+//		try {
+//			int myServingPort = applicationInfoManager.getInfo().getPort(); // read
+//																			// from
+//																			// my
+//																			// registered
+//																			// info
+//			ServerSocket serverSocket = new ServerSocket(myServingPort);
+//			final Socket s = serverSocket.accept();
+//			System.out.println("Client got connected... processing request from the client");
+//			processRequest(s);
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		System.out.println("Simulating service doing work by sleeping for " + 5 + " seconds...");
+//		try {
+//			Thread.sleep(5 * 1000);
+//		} catch (InterruptedException e) {
+//			// Nothing
+//		}
+	}
+
+	@PreDestroy
+	public void stop() {
+		if (eurekaClient != null) {
+			System.out.println("Shutting down server. Demo over.");
+			eurekaClient.shutdown();
+		}
+	}
+
+	private void waitForRegistrationWithEureka(EurekaClient eurekaClient) {
+		// my vip address to listen on
+		String vipAddress = configInstance.getStringProperty("eureka.vipAddress", "sampleservice.mydomain.net").get();
+		InstanceInfo nextServerInfo = null;
+		while (nextServerInfo == null) {
+			try {
+				nextServerInfo = eurekaClient.getNextServerFromEureka(vipAddress, false);
+			} catch (Throwable e) {
+				System.out.println("Waiting ... verifying service registration with eureka ...");
+
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void processRequest(final Socket s) {
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String line = rd.readLine();
+			if (line != null) {
+				System.out.println("Received a request from the example client: " + line);
+			}
+			String response = "BAR " + new Date();
+			System.out.println("Sending the response to the client: " + response);
+
+			PrintStream out = new PrintStream(s.getOutputStream());
+			out.println(response);
+
+		} catch (Throwable e) {
+			System.err.println("Error processing requests");
+		} finally {
+			if (s != null) {
+				try {
+					s.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	protected static final String PATH = "/livros";
 
@@ -35,7 +184,8 @@ public class LivroApi extends AbstractApi<LivroDto, Integer> {
 	@Override
 	@POST
 	@ApiOperation(value = "Adicionar um livro")
-	public Response adicionar(@ApiParam(value = "livroDto") LivroDto livroDto) throws EntidadeJaExisteExcecao, EntidadeNaoEncontradaExcecao, URISyntaxException {
+	public Response adicionar(@ApiParam(value = "livroDto") LivroDto livroDto)
+			throws EntidadeJaExisteExcecao, EntidadeNaoEncontradaExcecao, URISyntaxException {
 		return super.adicionar(livroDto);
 	}
 
@@ -57,7 +207,8 @@ public class LivroApi extends AbstractApi<LivroDto, Integer> {
 	@Override
 	@PUT
 	@ApiOperation(value = "Atualizar um livro", response = LivroDto.class)
-	public Response atualizar(@ApiParam(value = "livroDto") LivroDto livroDto) throws EntidadeNaoEncontradaExcecao, EntidadeJaExisteExcecao {
+	public Response atualizar(@ApiParam(value = "livroDto") LivroDto livroDto)
+			throws EntidadeNaoEncontradaExcecao, EntidadeJaExisteExcecao {
 		return super.atualizar(livroDto);
 	}
 
