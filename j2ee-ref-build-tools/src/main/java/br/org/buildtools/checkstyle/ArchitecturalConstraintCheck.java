5d1e66d63348d90a587ed73f8bdddb3c05fd0f24
Alisson Wilker <alisson.silva@tst.jus.br>
2017-09-28 19:54:58 -0300

    Novas regras arquiteturais.

--- j2ee-ref-build-tools/src/main/java/br/org/buildtools/checkstyle/ArchitecturalConstraintCheck.java
+++ j2ee-ref-build-tools/src/main/java/br/org/buildtools/checkstyle/ArchitecturalConstraintCheck.java
@@ -7,10 +7,13 @@ import static com.puppycrawl.tools.checkstyle.api.TokenTypes.INTERFACE_DEF;
 import static com.puppycrawl.tools.checkstyle.api.TokenTypes.PACKAGE_DEF;
 import static com.puppycrawl.tools.checkstyle.api.TokenTypes.STATIC_IMPORT;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class ArchitecturalConstraintCheck extends CustomCheck {
     
@@ -18,13 +21,39 @@ public class ArchitecturalConstraintCheck extends CustomCheck {
     
     private Collection<TipoArquitetural> tiposArquiteturais = new HashSet<TipoArquitetural>();
     
+    private String packageName;
+    // private String importName;
+    private String classOrInterfaceName;
+    
+    private PacoteArquitetural pacoteArquitetural;
+    
+    private SufixoArquitetural sufixoArquitetural;
+    
+    private Collection<AnotacaoArquitetural> anotacoesBlab;
+    
+    private TipoArquiteturalAbstrato pai;
+    
     public ArchitecturalConstraintCheck() {
         TipoArquitetural viewController = new TipoArquitetural(SufixoArquitetural.Controller,
-                PacoteArquitetural.ViewController);
-        // viewController.setAnotacoes(AnotacaoArquitetural.ManagedBean, AnotacaoArquitetural.ViewScoped);
+                PacoteArquitetural.ViewController, TipoArquiteturalAbstrato.AbstractController);
+        Collection<AnotacaoArquitetural> anotacoesArquiteturais = new HashSet<AnotacaoArquitetural>();
+        anotacoesArquiteturais.add(AnotacaoArquitetural.ManagedBean);
+        anotacoesArquiteturais.add(AnotacaoArquitetural.ViewScoped);
+        viewController.setAnotacoes(anotacoesArquiteturais);
         
+        // TipoArquitetural viewController = new TipoArquitetural(SufixoArquitetural.Controller,
+        // PacoteArquitetural.ViewController, TipoArquiteturalAbstrato.AbstractController,
+        // AnotacaoArquitetural.ManagedBean, AnotacaoArquitetural.ViewScoped);
+        
+        TipoArquitetural api = new TipoArquitetural(SufixoArquitetural.Api, PacoteArquitetural.Api,
+                TipoArquiteturalAbstrato.AbstractApi);
+        Collection<AnotacaoArquitetural> anotacoesArquiteturais2 = new HashSet<AnotacaoArquitetural>();
+//        anotacoesArquiteturais2.add(AnotacaoArquitetural.Path);
+//        anotacoesArquiteturais2.add(AnotacaoArquitetural.Api);
+//        api.setAnotacoes(anotacoesArquiteturais);
+        
+        tiposArquiteturais.add(api);
         tiposArquiteturais.add(viewController);
-        System.out.println("nova instancia");
     }
     
     @Override
@@ -42,20 +71,14 @@ public class ArchitecturalConstraintCheck extends CustomCheck {
         return getAcceptableTokens();
     }
     
-    private String packageName;
-    // private String importName;
-    private String classOrInterfaceName;
-    
-    private PacoteArquitetural pacoteArquitetural;
-    
-    private SufixoArquitetural sufixoArquitetural;
-    
     private void limpaAtributosInstancia() {
         packageName = null;
         // importName = null;
         classOrInterfaceName = null;
         pacoteArquitetural = null;
         sufixoArquitetural = null;
+        pai = null;
+        anotacoesBlab = new HashSet<AnotacaoArquitetural>();
     }
     
     @Override
@@ -92,10 +115,66 @@ public class ArchitecturalConstraintCheck extends CustomCheck {
             sufixoArquitetural = getSufixoArquitetural(classOrInterfaceName);
             
             System.out.println(sufixoArquitetural);
+            
+            DetailAST classModifiers = findFirstAstOfType(ast, TokenTypes.MODIFIERS);
+            if (classModifiers != null) {
+                List<DetailAST> astAnnotations = findAllAstsOfType(classModifiers, TokenTypes.IDENT);
+                if (astAnnotations != null) {
+                    for (DetailAST detailAST : astAnnotations) {
+                        if (detailAST.getParent().getType() == TokenTypes.ANNOTATION) {
+                            String anotacao = detailAST.getText();
+                            for (AnotacaoArquitetural anotacaoArquitetural : AnotacaoArquitetural.values()) {
+                                if (anotacaoArquitetural.name().equals(anotacao)) {
+                                    anotacoesBlab.add(anotacaoArquitetural);
+                                    System.out.println("@" + anotacaoArquitetural.name());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            DetailAST astExtends = findFirstAstOfType(ast, TokenTypes.EXTENDS_CLAUSE);
+            if (astExtends != null) {
+                String nomePai = findFirstAstOfType(astExtends, IDENT).getText();
+                System.out.println(nomePai);
+                
+                for (TipoArquiteturalAbstrato tipoArquiteturalAbstrato : TipoArquiteturalAbstrato.values()) {
+                    if (tipoArquiteturalAbstrato.name().equals(nomePai)) {
+                        pai = tipoArquiteturalAbstrato;
+                    }
+                }
+                
+            }
         }
         
         if (sufixoArquitetural != null && pacoteArquitetural != null) {
-            if (!tiposArquiteturais.contains(new TipoArquitetural(sufixoArquitetural, pacoteArquitetural))) {
+            TipoArquitetural ta = new TipoArquitetural(sufixoArquitetural, pacoteArquitetural, pai);
+            ta.setAnotacoes(anotacoesBlab);
+            System.out.println("printing-----");
+            System.out.println(sufixoArquitetural != null ? sufixoArquitetural.name() : "null");
+            System.out.println(pacoteArquitetural != null ? pacoteArquitetural.getNomePacote() : "null");
+            System.out.println(pai != null ? pai.name() : "null");
+            for (AnotacaoArquitetural anotacaoArquitetural : anotacoesBlab) {
+                System.out.println(anotacaoArquitetural != null ? anotacaoArquitetural.name() : "null");
+            }
+            System.out.println("------printing");
+            
+            System.out.println("#############");
+            for (TipoArquitetural tipo : tiposArquiteturais) {
+                System.out.println("um tipo");
+                System.out.println(tipo.getSufixo().name() != null ? tipo.getSufixo().name() : "null");
+                System.out.println(tipo.getPacote().getNomePacote() != null ? tipo.getPacote().getNomePacote() : "null");
+                System.out.println(tipo.getPai().name() != null ? tipo.getPai().name() : "null");
+                if (tipo.getAnotacoes() != null) {
+                    for (AnotacaoArquitetural anotacaoArquitetural : tipo.getAnotacoes()) {
+                        System.out.println(anotacaoArquitetural != null ? anotacaoArquitetural.name() : "null");
+                    }
+                }
+            }
+            System.out.println("#############");
+            
+            if (!tiposArquiteturais.contains(ta)) {
                 System.out.println("não está conforme a arquitetura");
             } else {
                 System.out.println("está conforme a arquitetura");
@@ -160,7 +239,7 @@ enum PacoteArquitetural {
 }
 
 enum AnotacaoArquitetural {
-    ManagedBean, ViewScoped;
+    ManagedBean, ViewScoped;//, Path, Api;
 }
 
 enum InterfaceArquitetural {
@@ -171,10 +250,16 @@ enum SufixoArquitetural {
     Dto, Controller, Api;
 }
 
+enum TipoArquiteturalAbstrato {
+    AbstractController, AbstractApi;
+}
+
 class TipoArquitetural {
     private SufixoArquitetural sufixo;
     private PacoteArquitetural pacote;
-    // private AnotacaoArquitetural[] anotacoes;
+    private Collection<AnotacaoArquitetural> anotacoes;
+    private TipoArquiteturalAbstrato pai;
+    
     // private Collection<InterfaceArquitetural> interfaces;
     //
     // public TipoArquitetural(SufixoArquitetural sufixo, PacoteArquitetural pacote, AnotacaoArquitetural[] anotacoes,
@@ -184,24 +269,50 @@ class TipoArquitetural {
     // this.interfaces = interfaces;
     // }
     //
-    // public void setAnotacoes(AnotacaoArquitetural... anotacoes) {
-    // this.anotacoes = anotacoes;
-    // }
+    public void setAnotacoes(Collection<AnotacaoArquitetural> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
     
-    public TipoArquitetural(SufixoArquitetural controller, PacoteArquitetural viewcontroller) {
+    public TipoArquitetural(SufixoArquitetural controller, PacoteArquitetural viewcontroller,
+            TipoArquiteturalAbstrato pai) {
         this.sufixo = controller;
         this.pacote = viewcontroller;
+        this.pai = pai;
     }
+    
+//    public TipoArquitetural(SufixoArquitetural controller, PacoteArquitetural viewcontroller,
+//            TipoArquiteturalAbstrato pai, AnotacaoArquitetural... anotacoes) {
+//        this(controller, viewcontroller, pai);
+//        setAnotacoes(Arrays.asList(anotacoes));
+//    }
     
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + ((anotacoes == null) ? 0 : anotacoes.hashCode());
         result = prime * result + ((pacote == null) ? 0 : pacote.hashCode());
+        result = prime * result + ((pai == null) ? 0 : pai.hashCode());
         result = prime * result + ((sufixo == null) ? 0 : sufixo.hashCode());
         return result;
     }
     
+    public SufixoArquitetural getSufixo() {
+        return sufixo;
+    }
+
+    public PacoteArquitetural getPacote() {
+        return pacote;
+    }
+
+    public Collection<AnotacaoArquitetural> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public TipoArquiteturalAbstrato getPai() {
+        return pai;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -211,7 +322,17 @@ class TipoArquitetural {
         if (getClass() != obj.getClass())
             return false;
         TipoArquitetural other = (TipoArquitetural) obj;
+        if (anotacoes == null) {
+            if (other.anotacoes != null)
+                return false;
+            System.out.println("anotacoes de ambos igual a null");
+        } else if (!anotacoes.equals(other.anotacoes)) {
+            System.out.println("anotacoes nao sao iguais");
+            return false;
+        }
         if (pacote != other.pacote)
+            return false;
+        if (pai != other.pai)
             return false;
         if (sufixo != other.sufixo)
             return false;
